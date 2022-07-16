4b57f019d4bb5d5486970c717f8a44dc4463b68b
nao_ <67215204+naoakitakado@users.noreply.github.com>
2022-07-16 10:18:08 +0900

    jisen

---
+++ src/router/Router.tsx
@@ -0,0 +1,10 @@
+import { memo, VFC } from "react";
+import { Route, Switch } from "react-router-dom";
+
+export const Router: VFC = memo(() => {
+  return (
+    <Switch>
+      <Route></Route>
+    </Switch>
+  );
+});
